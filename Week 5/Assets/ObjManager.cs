679fc313ff6c602a21f67d8c6701b2ea62abe9cb
Korey019 <1027634@student.sae.edu.au>
2022-03-16 15:33:12 +1100

    fixed errors from week 5

--- Week 5/Assets/ObjManager.cs
+++ Week 5/Assets/ObjManager.cs
@@ -12,10 +12,9 @@ public class ObjManager : MonoBehaviour
 
     private List<GameObject> spawnedObjs = new List<GameObject>();
     private bool canTick = true;
-    
     // Update is called once per frame
     void Update()
-   {
+    {
         if (canTick)
         {
             if (spawnedObjs.Count < amount)
@@ -25,7 +24,7 @@ public class ObjManager : MonoBehaviour
                 Vector3 pos = new Vector3(transform.position.x + randX,
                                           transform.position.y,
                                           transform.position.z + randZ);
-                spawnedObjs.Add(Instantiate(shapes[Random.Range (0, shapes.Length)], pos, transform.rotation));
+                spawnedObjs.Add(Instantiate(shapes[Random.Range(0, shapes.Length)], pos, transform.rotation));
                 int rand = Random.Range(0, mats.Length);
                 Material newMat = mats[rand];
                 spawnedObjs[spawnedObjs.Count - 1].tag = tags[rand];
@@ -35,41 +34,46 @@ public class ObjManager : MonoBehaviour
             StartCoroutine(Timer(secDelay));
         }
         UpdateObjs();
-   }
+    }
 
     IEnumerator Timer(float delay)
     {
-        /*
-        yield return new WaitForSeconds(delay);
-        canTick = true;
+        //anything here runs immediately
 
-        int phase = 0;
-        phase++;
-        if(phase == 4)
+        //this line, creates the delay
+        yield return new WaitForSeconds(delay);
+        //anything here, waits the delay
+
+        canTick = true;
+        /*
+        //Hint for brief 2
+        int phase = 0; //0 default, setup phase
+        phase++; //phase would increase, next phase might pick fighters
+        if(phase == 4) //if we hit the last phaase of our battle simulator, reset it
         {
-            phase = 0
+            phase = 0;
         }
         switch(phase);
         */
-    }
+    } 
 
     private void UpdateObjs()
     {
-        foreach(GameObject obj in spawnObjs)
+        foreach (GameObject obj in spawnedObjs)
         {
-            switch(obj.tag);
+            switch (obj.tag)
             {
-                case "Orange"; //Go fwd
+                case "Orange": //Go fwd
                     obj.transform.position += Vector3.forward * Time.deltaTime;
                     break;
-                case "Blue"; //Go up
+                case "Blue": //Go up
                     obj.transform.position += Vector3.up * Time.deltaTime;
                     break;
-                case "Yellow"; //Go down
-                    obj.transform.Position += Vector3.down * Time.deltaTime;
+                case "Yellow": //Go down
+                    obj.transform.position += Vector3.down * Time.deltaTime;
                     break;
-                case "Green"; //Grow
-                    obj.transform.Position += Vector3.one * Time.deltaTime;
+                case "Green": //Grow
+                    obj.transform.localScale += Vector3.one * Time.deltaTime;
                     break;
             }
         }
