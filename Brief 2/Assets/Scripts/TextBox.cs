aacdaa3c0f4002d104e19b384b4c8f583792c81e
Korey019 <1027634@student.sae.edu.au>
2022-03-24 18:13:49 +1100

    week 7 fixed @6:09pm

--- Brief 2/Assets/Scripts/TextBox.cs
+++ Brief 2/Assets/Scripts/TextBox.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextBox : MonoBehaviour
 {
+        public TextBox textComp;
+        private string curText;
+        public List<string> textLines;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComp = GetComponent<TextBox>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NewMessage(string message)
     {
-        
+        //check is we already have 4 lines
+        if (textLines.Count >= 4)
+        {
+            //if so, make some room for a new line
+            textLines.RemoveAt(0);
+        }
+        // "\n" creates a new line
+        textLines.Add(message + "\n");
+        //reset our curText value
+        curText = "";
+        //add each of our textLines to our curText variable
+        for (int i = 0; i < textLines.Count; i++)
+        {
+            curText += textLines[i];
+        }
+        //now set the text variable on our textbox to display our text
+        textComp.textComp = curText;
     }
 }
