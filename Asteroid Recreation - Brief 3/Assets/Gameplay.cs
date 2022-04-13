012dfec65d020f7f4461d65a923babe923f3b59c
Korey019 <1027634@student.sae.edu.au>
2022-04-14 00:55:25 +1000

    asteroid adjustments

--- Asteroid Recreation - Brief 3/Assets/Gameplay.cs
+++ Asteroid Recreation - Brief 3/Assets/Gameplay.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class Gameplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject asteroid;
+    public GameObject rocket;
+    public int _startLevelAsteriodsNum;
+    private bool _allAsteriodsOffScreen;
+    private int levelAsteroidNum;
+    private Camera mainCam;
+    private int asteriodLife;
+
+    private void Start()
     {
-        
+        levelAsteroidNum.SetActive(false);
+        mainCam = Camera.main;
+        _startLevelAsteroidsNum = 2;
+        CreateAsteroids(-startLevelAsteroidsNum);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * 0.8f);
+
+        if (asteroidLife <= 0)
+        {
+            asteroidLife = 6;
+            CreateAsteroids(1);
+        }
+
+        float sceneWidth = mainCam.orthographicSize * 2 * mainCam.aspect;
+        float sceneHeight = mainCam.orthographicSize * 2;
+        float sceneRightEdge = sceneRightEdge * -1;
+        float sceneTopEdge = sceneHeight / 2;
+        float sceneBottomEdge = sceneTopEdge * -1;
+
+        _allAsteroidsOffScreen = true;
+
     }
+
+    private void CreateAsteroids(float asteroidsNum)
+    {
+        for (int i = 1; i <= asteroidsNum; i++)
+        {
+            GameObject AsteroidClone = Instantiate(asteroidsNum, new Vector2(Random.Range(-10, 10), 6f), transform.rotation);
+            AsteroidClone.GetComponent().SetGeneration(1);
+            AsteroidClone.SetActive(true);
+        }
+    }
+
+    public void RocketFail()
+    {
+        Cursor.visible = true;
+        print("GAME OVER");
+    }
+
+    public void asteroidDestroyed()
+    {
+        asteroidLife--;
+    }
+
+    public int startLevelAsteroidsNum
+    {
+        get { return _startLevelAsteroidsNum; }
+    }
+
+    public bool allAsteroidsOffScreen
+    {
+        get { return _allAsteroidsOffScreen; }
+    }
+
 }
